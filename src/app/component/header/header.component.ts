a9b16faff6407dfcf3a6d6ec74727ec43de5aafd
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-05 20:50:22 +0500

    Sync

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  private _cart: Cart = { items: [] }
+  itemsQuantity = 0;
+  constructor(private cartService: CartService) { }
+  @Input()
+  get cart(): Cart {
+    return this._cart
+  }
+  set cart(cart: Cart) {
+    this._cart = cart;
+    this.itemsQuantity = cart.items.map((item) => item.quantity).reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  }
 
-  constructor() { }
 
-  ngOnInit(): void {
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items)
+  }
+
+
+  onClearCart() {
+    this.cartService.clearCart();
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,22 +7,71 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   providedIn: 'root'
 })
 export class CartService {
-  cart=new BehaviorSubject<Cart>({items:[]})
+  cart = new BehaviorSubject<Cart>({ items: [] })
 
-  constructor(private _snackBar:MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar) { }
 
 
-  addToCart(item: CartItem):void{
-    const items=[...this.cart.value.items];
+  addToCart(item: CartItem): void {
+    const items = [...this.cart.value.items];
 
-    const itemsCart=items.find((_item)=>{
-      _item.id===item.id
+    const existingItem = items.find((_item) => _item.id === item.id);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      items.push(item);
+    }
+
+    this.cart.next({ items });
+    this._snackBar.open('1 item added to cart', 'ok', { duration: 3000 });
+  }
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined
+    let filteredItem = this.cart.value.items.map((_item) => {
+
+      if (_item.id === item.id) {
+        _item.quantity--;
+        if (_item.quantity === 0) {
+          itemForRemoval = _item
+
+        }
+      }
+      return _item;
+    })
+    if (itemForRemoval) {
+      this.cart.next({ items: filteredItem })
+      filteredItem = this.removeFromCart(itemForRemoval, false)
+    }
+    this._snackBar.open('1 item removed from Cart', 'Ok', { duration: 3000 })
+
+  }
+
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+  }
+
+
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open('Cart is Cleared', 'Ok', {
+      duration: 3000
     })
 
-    if(itemsCart){
-      itemsCart.quantity+=1
-    } else{
-      items.push(item)
+  }
+
+
+  removeFromCart(item: CartItem, update = true) {
+    const filterItems = this.cart.value.items.filter((_item) => _item.id !== item.id)
+
+    if (update) {
+      this.cart.next({ items: filterItems })
+      this._snackBar.open('1 item removed from Cart', 'Ok', { duration: 3000 })
     }
+
+    return filterItems
   }
 }
