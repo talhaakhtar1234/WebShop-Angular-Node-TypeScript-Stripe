ecc9400892904442260a888a122c1719a77dbd80
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-05 14:34:55 +0500

    SYNC

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -8,23 +8,31 @@ import { Cart, CartItem } from 'src/app/models/cart.model'
 export class CartComponent implements OnInit {
   cart: Cart = {
     items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'Snickers',
+        price: 150,
+        quantity: 3,
+        id: 1
+      },
 
-      // {
-      //   product: 'https://via.placeholder.com/150',
-      //   name: 'Snickers',
-      //   price: 150,
-      //   quantity: 1,
-      //   id: 1
-      // }
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'Snickers',
+        price: 150,
+        quantity: 1,
+        id: 2
+      }
 
     ]
   }
-  dataSource:Array<CartItem>=[];
-  displayedColumn: Array<string>=[
+  dataSource: Array<CartItem> = [];
+  displayedColumn: Array<string> = [
     'product',
     'name',
     'price',
     'quantity',
+    'total',
     'action'
   ]
 
@@ -32,7 +40,13 @@ export class CartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.dataSource=this.cart.items
+    this.dataSource = this.cart.items
   }
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, curr) => {
+       return prev + curr;
+    }, 0);
+ }
+
 
 }
