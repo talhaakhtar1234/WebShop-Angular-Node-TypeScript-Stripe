2d27c28e2d609fbb7305874738c23659647766ae
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-05 15:29:25 +0500

    Changes

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Cart } from '../models/cart.model';
+import { Cart, CartItem } from '../models/cart.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,20 @@ import { Cart } from '../models/cart.model';
 export class CartService {
   cart=new BehaviorSubject<Cart>({items:[]})
 
-  constructor() { }
+  constructor(private _snackBar:MatSnackBar) { }
+
+
+  addToCart(item: CartItem):void{
+    const items=[...this.cart.value.items];
+
+    const itemsCart=items.find((_item)=>{
+      _item.id===item.id
+    })
+
+    if(itemsCart){
+      itemsCart.quantity+=1
+    } else{
+      items.push(item)
+    }
+  }
 }
